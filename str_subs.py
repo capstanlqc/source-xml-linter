3153607d9bc06526f2f1a6b3f28107f210faa465
Manuel Souto Pico <m.soutopico@gmail.com>
2024-05-20 13:13:02 +0200

    Fixed regex rule that removed part in parenthesis in key PST492_bbc9e6cf3a7dd22a63515ab6adf17eca_8 in file 27_QQSP_N/PISA_2025FT_QQ_STQPBA-StudentQuestionnairePBA.xml

--- str_subs.py
+++ str_subs.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys, os
 from datetime import datetime
+
 startTime = datetime.now()
 import regex as re
+
 # import re
 import argparse
 import pandas as pd
-#import pprint
+
+# import pprint
 
 
 # install dependencies
@@ -16,8 +19,11 @@ import pandas as pd
 
 
 log = []
+
+
 def add_to_log(line):
     log.append(line)
+
 
 # ############# PROGRAM DESCRIPTION ###########################################
 
@@ -25,15 +31,21 @@ text = "String substitution in XML files"
 
 # initialize arg parser with a description
 parser = argparse.ArgumentParser(description=text)
-parser.add_argument("-V", "--version", help="show program version",
-                    action="store_true")
-#parser.add_argument("-p", "--params", help="specify path to list of patterns")
+parser.add_argument("-V", "--version", help="show program version", action="store_true")
+# parser.add_argument("-p", "--params", help="specify path to list of patterns")
 parser.add_argument(
-    "-i", "--input", help="specify path to the folder containing the files to be processed")
+    "-i",
+    "--input",
+    help="specify path to the folder containing the files to be processed",
+)
 parser.add_argument(
-    "-o", "--output", help="specify path to the folder where the processed files should be saved")
+    "-o",
+    "--output",
+    help="specify path to the folder where the processed files should be saved",
+)
 parser.add_argument(
-    "-c", "--config", help="specify path to the config file containing patterns etc.")
+    "-c", "--config", help="specify path to the config file containing patterns etc."
+)
 
 # read arguments from the command cur_str
 args = parser.parse_args()
@@ -45,44 +57,49 @@ if args.version:
     sys.exit()
 
 if args.input and args.output and args.config:
-    input_dpath = args.input.rstrip('/')
-    output_dpath = args.output.rstrip('/')
+    input_dpath = args.input.rstrip("/")
+    output_dpath = args.output.rstrip("/")
     config_fpath = args.config.strip()
 else:
-    print("Some required argument not found. Run this script with `--help` for details.")
+    print(
+        "Some required argument not found. Run this script with `--help` for details."
+    )
     sys.exit()
 
 parent_dir = os.path.dirname(os.path.realpath(__file__))
 log_dpath = os.path.join(parent_dir, "logs")
 os.makedirs(log_dpath, exist_ok=True)
-log_file = os.path.join(log_dpath, "log.txt") # @todo: add timestamp to log file
+log_file = os.path.join(log_dpath, "log.txt")  # @todo: add timestamp to log file
 
 
 # ############# FUNCTIONS ###########################################
+
 
 def load_patterns(config_fpath, sheets):
     """Create dictionary with search and replace regex patterns"""
 
     # @todo: check whether file exists, quit if not
     for sheet in sheets:
-        #df = pd.read_excel(config_fpath)
-        patterns_df = pd.read_excel(config_fpath, sheet_name=sheet).fillna('')
+        # df = pd.read_excel(config_fpath)
+        patterns_df = pd.read_excel(config_fpath, sheet_name=sheet).fillna("")
         patterns = dict(zip(patterns_df.search, patterns_df.replac))
     return patterns
 
 
 def get_text(fpath):
     """Get all text content from file"""
-    with open(fpath, 'r') as f:
+    with open(fpath, "r") as f:
         return f.read()
 
 
 def get_backref_values(match, search_pattern, replace_pattern):
     """Get list of backreferences (e.g. $1, $2, etc.) used in replacement pattern"""
 
-    captured_groups = [match.group(i) for i in range(1, search_pattern.groups+1)] # list of strings
+    captured_groups = [
+        match.group(i) for i in range(1, search_pattern.groups + 1)
+    ]  # list of strings
 
-    if len(captured_groups) != 0: # @dev: needed?
+    if len(captured_groups) != 0:  # @dev: needed?
         backreferences = {f"${i}": elem for i, elem in enumerate(captured_groups, 1)}
 
         return backreferences
@@ -91,19 +108,19 @@ def get_backref_values(match, search_pattern, replace_pattern):
 
 
 def recursive_convert_backref_style(replace_pattern):
-    """Replaces $<n> style with \<n> for backreferences in replacement patterns."""
-    
+    """Replaces $<n> style with \\<n> for backreferences in replacement patterns."""
+
     for match in re.finditer(r"\$(?=\d+)", replace_pattern):
-        replace_pattern = re.sub(fr'\{match.group(0)}', '\\\\', replace_pattern)
-    
+        replace_pattern = re.sub(rf"\{match.group(0)}", "\\\\", replace_pattern)
+
     return replace_pattern
 
 
 def log_changes(search_pattern, replace_pattern, text):
 
     for match in re.finditer(search_pattern, text):
-    
-        orig_text = text[:] # not used
+
+        orig_text = text[:]  # not used
         full_match = match.group(0)
         backreferences = get_backref_values(match, search_pattern, replace_pattern)
         replace_value = replace_pattern[:]
@@ -113,36 +130,38 @@ def log_changes(search_pattern, replace_pattern, text):
             replace_value = replace_value.replace(k, v)
 
         # text = text.replace(full_match, replace_value) # replacing literal strings
-        
-        # logging... 
-        add_to_log(f"{search_pattern=}") # only interesting for debugging
-        add_to_log(f"{full_match=}") # only interesting for debugging
-        add_to_log(f"{replace_pattern=}") # only interesting for debugging
-        add_to_log(f"{backreferences=}") # only interesting for debugging
-        add_to_log(f"{replace_value=}") # only interesting for debugging
-        add_to_log(f"👉 Text matched: '{full_match}'")
-        add_to_log(f"👉 Replace with: '{replace_value}'")          
-        
-        add_to_log("--------------------------------------------------------------------------------")
 
-    
+        # logging...
+        add_to_log(f"{search_pattern=}")  # only interesting for debugging
+        add_to_log(f"{full_match=}")  # only interesting for debugging
+        add_to_log(f"{replace_pattern=}")  # only interesting for debugging
+        add_to_log(f"{backreferences=}")  # only interesting for debugging
+        add_to_log(f"{replace_value=}")  # only interesting for debugging
+        add_to_log(f"👉 Text matched: '{full_match}'")
+        add_to_log(f"👉 Replace with: '{replace_value}'")
+
+        add_to_log(
+            "--------------------------------------------------------------------------------"
+        )
+
+
 def run_substitutions(text, patterns, flags=re.DOTALL):
     """Do the substitutions in the text"""
 
     for k, v in patterns.items():
-        
-        if not k or not isinstance(k, str): # to avoid None and nan
+
+        if not k or not isinstance(k, str):  # to avoid None and nan
             continue
-            
-        search_pattern = re.compile(k, re.MULTILINE|re.DOTALL)
-        replace_pattern = fr"{recursive_convert_backref_style(v)}" # or: r"%s" % v
+
+        search_pattern = re.compile(k, re.MULTILINE | re.DOTALL)
+        replace_pattern = rf"{recursive_convert_backref_style(v)}"  # or: r"%s" % v
 
         orig_text = text[:]
         text = re.sub(search_pattern, replace_pattern, text)
 
         if orig_text != text:
             log_changes(search_pattern, v, orig_text)
-        
+
     return text
 
 
@@ -153,7 +172,7 @@ def create_output_dpath(dpath):
         add_to_log(f"Directory {dpath} already exists")
     else:
         try:
-            os.mkdir(dpath) # , access_rights)
+            os.mkdir(dpath)  # , access_rights)
         except OSError:
             add_to_log("Error: Creation of directory %s failed" % dpath)
         else:
@@ -164,7 +183,7 @@ def write_edited_file(fpath, text_content):
     """Create output file and all directories in the path leading to the file"""
 
     os.makedirs(os.path.dirname(fpath), exist_ok=True)
-    with open(fpath, "w", encoding='utf-8') as f:
+    with open(fpath, "w", encoding="utf-8") as f:
         f.write(text_content)
 
 
@@ -172,11 +191,11 @@ def write_edited_file(fpath, text_content):
 
 if __name__ == "__main__":
 
-    patterns =  load_patterns(config_fpath, sheets=['patterns']) # dictionary
+    patterns = load_patterns(config_fpath, sheets=["patterns"])  # dictionary
 
-    # @todo: check that input_dpath exists and stop if it doesn't 
+    # @todo: check that input_dpath exists and stop if it doesn't
     create_output_dpath(output_dpath)
- 
+
     for dirpath, dirs, files in os.walk(input_dpath):
 
         for fname in files:
@@ -186,8 +205,9 @@ if __name__ == "__main__":
 
             # paths and names
             add_to_log("")
-            add_to_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            
+            add_to_log(
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+            )
 
             add_to_log(f"👉 Processing file {fname}... ")
             input_fpath = os.path.join(dirpath, fname)
@@ -196,12 +216,14 @@ if __name__ == "__main__":
 
             # processing
             text = get_text(input_fpath)
-            text = run_substitutions(text, patterns, re.MULTILINE|re.DOTALL) # |re.UNICODE
-             
+            text = run_substitutions(
+                text, patterns, re.MULTILINE | re.DOTALL
+            )  # |re.UNICODE
+
             ## output
             write_edited_file(output_fpath, text)
 
             # log
             add_to_log(f"----\nIt took: {datetime.now() - startTime}")
-            with open(log_file, 'w') as f:
-                f.write('\n'.join(log))
+            with open(log_file, "w") as f:
+                f.write("\n".join(log))
